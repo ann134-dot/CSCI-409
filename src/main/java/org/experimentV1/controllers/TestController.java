a12c29d339495f99701c5c4181ecb95a48408f31
Anara Zhangbyrshyn <anara.zhangbyrshyn@nu.edu.kz>
2023-03-18 21:37:27 +0600

    added /hello controller

--- src/main/java/org/experimentV1/controllers/TestController.java
+++ src/main/java/org/experimentV1/controllers/TestController.java
@@ -1,2 +1,13 @@
-package org.experimentV1.controllers;public class TestController {
+package org.experimentV1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Testing, Hello!";
+    }
 }
--- src/main/java/org/experimentV1/repositories/ExperimentRepository.java
+++ src/main/java/org/experimentV1/repositories/ExperimentRepository.java
@@ -4,7 +4,6 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import org.experimentV1.entity.Experiment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
