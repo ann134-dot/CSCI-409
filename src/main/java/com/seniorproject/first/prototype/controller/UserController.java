6a37a70b562642ce52b0f8e0f093b6ec50a5a639
zhaqsylyq <71256573+zhaqsylyq@users.noreply.github.com>
2023-04-06 18:27:58 +0600

    Add files via upload

--- src/main/java/com/seniorproject/first/prototype/controller/UserController.java
+++ src/main/java/com/seniorproject/first/prototype/controller/UserController.java
@@ -4,14 +4,13 @@ import com.seniorproject.first.prototype.entity.Experiment;
 import com.seniorproject.first.prototype.entity.User;
 import com.seniorproject.first.prototype.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v2/user")
 public class UserController {
 
     @Autowired
@@ -24,6 +23,11 @@ public class UserController {
     @GetMapping("/my-created-experiments")
     public List<Experiment> fetchMyCreatedExperiments(){ // move this to the experiment controller
         return userService.fetchMyCreatedExperiments();
+    }
+
+    @GetMapping("/myUserInfo")
+    public ResponseEntity<Object> getMyUserInfo(){
+        return userService.getMyUserInfo();
     }
 
 }
