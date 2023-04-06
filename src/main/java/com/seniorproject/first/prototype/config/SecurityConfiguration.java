0abae6c34f0ab22ec4028bf0e78293038f5351c9
anara <anara.zhangbyrshyn@nu.edu.kz>
2023-04-06 23:19:48 +0600

    created DatabaseSeeder User test@gmail.com

--- src/main/java/com/seniorproject/first/prototype/config/SecurityConfiguration.java
+++ src/main/java/com/seniorproject/first/prototype/config/SecurityConfiguration.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -21,7 +25,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler; //spring automatically finds our implementation
-
+    private final PasswordEncoder passwordEncoder;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -46,4 +50,16 @@ public class SecurityConfiguration {
                 .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
         return http.build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("test@gmail.com")
+//                        .password(passwordEncoder.encode("1234"))
+//                        .roles("USER")
+//                        .build()
+//        );
+//    }
+
+
 }
---
+++ src/main/java/com/seniorproject/first/prototype/util/DatabaseSeeder.java
@@ -0,0 +1,51 @@
+package com.seniorproject.first.prototype.util;
+
+import com.seniorproject.first.prototype.entity.Experiment;
+import com.seniorproject.first.prototype.entity.Role;
+import com.seniorproject.first.prototype.entity.User;
+import com.seniorproject.first.prototype.repository.ExperimentRepository;
+import com.seniorproject.first.prototype.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+@Component
+@Slf4j
+public class DatabaseSeeder implements CommandLineRunner {
+    private final ExperimentRepository experimentRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public DatabaseSeeder(ExperimentRepository experimentRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.experimentRepository = experimentRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Experiment experiment;
+        try {
+            User user = User.builder()
+                    .userEmail("test@gmail.com")
+                    .age(22L)
+                    .gender("m")
+                    .degree("UG")
+                    .firstName("Test")
+                    .lastName("Testov")
+                    .role(Role.ADMIN)
+                    .password(passwordEncoder.encode("1234"))
+                    .build();
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.error("error in creating a user: {}", e.getMessage());
+        }
+
+
+    }
+}
+
