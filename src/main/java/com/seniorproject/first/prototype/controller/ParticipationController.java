8dfab721762f052860abfdc8d263e2a3757f08b3
Zhaksylyk <zhaksylyk.chalgimbayev@nu.edu.kz>
2023-04-08 23:12:17 +0600

    changed the return types for postJoin and getMyParticipationRequests back to ResponseEntity<Object>, now it is working wtf

--- src/main/java/com/seniorproject/first/prototype/controller/ParticipationController.java
+++ src/main/java/com/seniorproject/first/prototype/controller/ParticipationController.java
@@ -30,7 +30,7 @@ public class ParticipationController {
 
     //joining the experiment
     @PostMapping("/join/{id}")
-    public Participation postJoin(@PathVariable("id") Long experimentId) throws Exception {
+    public ResponseEntity<Object> postJoin(@PathVariable("id") Long experimentId) throws Exception {
         return participationService.postJoin(experimentId);
     }
     @GetMapping("/myCreatedExperiments/pending-requests/{id}")
@@ -64,7 +64,7 @@ public class ParticipationController {
     }
 
     @GetMapping("/myRequests")
-    public List<Participation> getMyParticipationRequests(){
+    public ResponseEntity<Object> getMyParticipationRequests(){
         return participationService.getMyParticipationRequests();
     }
 
